94b2deac6f19db3777371645d8c3d498fa304b4d
Meeraj Kunnumpurath <mkunnumpurath@gmail.com>
2011-12-04 22:38:06 +0000

    First commit

---
+++ scala-conduit-server/src/test/scala/org/scalaconduit/server/junit.scala
@@ -0,0 +1,17 @@
+package org.scalaconduit.server
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
