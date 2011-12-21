bcfdc0f7cac093eab824d972c2559b2805b5f6d4
Meeraj Kunnumpurath <mkunnumpurath@gmail.com>
2011-12-21 08:14:11 +0000

    Changed var to val using recursive call

--- scala-conduit-spi/src/main/scala/org/scalaconduit/spi/Splitter.scala
+++ scala-conduit-spi/src/main/scala/org/scalaconduit/spi/Splitter.scala
@@ -1,23 +1,25 @@
 package org.scalaconduit.spi
 import org.w3c.dom.Document
 import org.w3c.dom.NodeList
-
 import javax.xml.xpath.XPathConstants
 import javax.xml.xpath.XPathFactory
+import org.w3c.dom.Node
 
 case class Splitter(payload: AnyRef) {
 
-    def --< (expression: String): List[AnyRef] = {
+    def --<(expression: String): List[AnyRef] = {
 
-        var list: List[AnyRef] = List()
         if (payload.isInstanceOf[Document]) {
             val xpath = XPathFactory.newInstance().newXPath()
             val nodeList = xpath.evaluate(expression, payload.asInstanceOf[Document], XPathConstants.NODESET).asInstanceOf[NodeList]
-            for (i <- 0 until nodeList.getLength()) {
-                list = nodeList.item(i) :: list
+            def convert(nodeList: NodeList, list: List[Node]) : List[Node] = {
+                if (nodeList.getLength() == list.size) list
+                else convert(nodeList, nodeList.item(list.size) :: list)
             }
+            convert(nodeList, Nil)
+        } else {
+        	Nil
         }
-        list
 
     }
 
